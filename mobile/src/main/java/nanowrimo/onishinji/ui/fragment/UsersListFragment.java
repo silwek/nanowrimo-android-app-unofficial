a4a7f7fba9b2e725e39629ff538a4ccbe3f92d59
Amandine Ferrand <amandine.ferrand@freemo.blue>
2015-10-11 19:00:47 +0200

    fix style

--- mobile/src/main/java/nanowrimo/onishinji/ui/fragment/UsersListFragment.java
+++ mobile/src/main/java/nanowrimo/onishinji/ui/fragment/UsersListFragment.java
@@ -1,75 +1,28 @@
 package nanowrimo.onishinji.ui.fragment;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.android.volley.Cache;
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.interfaces.OnChartValueSelectedListener;
-import com.github.mikephil.charting.utils.LargeValueFormatter;
-import com.github.mikephil.charting.utils.LimitLine;
-import com.github.mikephil.charting.utils.ValueFormatter;
-import com.github.mikephil.charting.utils.XLabels;
 import com.squareup.otto.Subscribe;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Array;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
 
 import nanowrimo.onishinji.R;
 import nanowrimo.onishinji.model.BusManager;
-import nanowrimo.onishinji.model.Database;
 import nanowrimo.onishinji.model.Friend;
 import nanowrimo.onishinji.model.Friends;
-import nanowrimo.onishinji.model.Historic;
-import nanowrimo.onishinji.model.HttpClient;
-import nanowrimo.onishinji.model.User;
 import nanowrimo.onishinji.ui.activity.FriendActivity;
-import nanowrimo.onishinji.ui.activity.FriendsActivity;
-import nanowrimo.onishinji.ui.widget.MyBarMarkerView;
-import nanowrimo.onishinji.ui.widget.MyMarkerView;
-import nanowrimo.onishinji.ui.widget.WordCountProgress;
-import nanowrimo.onishinji.utils.StringUtils;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -112,7 +65,7 @@ public class UsersListFragment extends Fragment {
         mListView = (ListView) getView().findViewById(R.id.listView);
 
         mItems = new ArrayList<Friend>();
-        mAdapter = new ArrayAdapter<Friend>(getActivity(), android.R.layout.simple_list_item_1, mItems);
+        mAdapter = new ArrayAdapter<Friend>(getActivity(), R.layout.item_friend, mItems);
 
         mListView.setAdapter(mAdapter);
 
