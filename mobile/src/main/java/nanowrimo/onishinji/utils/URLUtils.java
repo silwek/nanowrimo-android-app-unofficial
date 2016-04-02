b5f7fae79153f90930eb0df988e7a4a9e5b1d9d6
Amandine Ferrand <amandine.ferrand@freemo.blue>
2016-04-02 21:20:04 +0200

    Add time zone in URL

--- mobile/src/main/java/nanowrimo/onishinji/utils/URLUtils.java
+++ mobile/src/main/java/nanowrimo/onishinji/utils/URLUtils.java
@@ -1,5 +1,9 @@
 package nanowrimo.onishinji.utils;
 
+import android.net.Uri;
+
+import java.util.Calendar;
+
 import nanowrimo.onishinji.model.WritingSession;
 
 /**
@@ -7,18 +11,33 @@ import nanowrimo.onishinji.model.WritingSession;
  */
 public class URLUtils {
 
-    final static String baseUrlCamp = "https://campnanowrimo.herokuapp.com/users/";
-    final static String baseUrlNano = "https://nanowrimo.herokuapp.com/users/";
+    protected final static String baseUrlCamp = "https://campnanowrimo.herokuapp.com/users/";
+    protected final static String baseUrlNano = "https://nanowrimo.herokuapp.com/users/";
     public final static String WRITE_API = "http://nanowrimo.org/api/wordcount";
 
     public static String getUserUrl(int type, String username) {
+        return addTimeZone(getBaseUserUrl(type, username));
+    }
+
+    public static String getHistoryUserUrl(int type, String username) {
+        return addTimeZone(getBaseUserUrl(type, username) + "/history");
+    }
+
+    public static String getFriendUserUrl(int type, String username) {
+        final String baseUrl = getBaseUserUrl(type, username);
+        return addTimeZone(baseUrl + "/friends");
+    }
+
+    protected static String getBaseUserUrl(int type, String username) {
         final String baseUrl = type == WritingSession.NANOWRIMO ? baseUrlNano : baseUrlCamp;
         return baseUrl + StringUtils.encodeUserNameForApi(username);
     }
 
-    public static String getFriendUserUrl(int type, String username) {
-        final String baseUrl = type == WritingSession.NANOWRIMO ? baseUrlNano : baseUrlCamp;
-        return baseUrl + StringUtils.encodeUserNameForApi(username) + "/friends";
+    protected static String addTimeZone(String url) {
+        Uri uri = Uri.parse(url);
+        Calendar c = Calendar.getInstance();
+        String timeZone = c.getTimeZone().getID();
+        return uri.buildUpon().appendQueryParameter("timeZone", timeZone).build().toString();
     }
 
 }
